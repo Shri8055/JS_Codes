97a1d05da4365da5f952865a823415a9e436231d
Shrinivas Mahendra Kangralkar <88592860+Shri8055@users.noreply.github.com>
2024-08-25 18:39:38 +0530

    Add files via upload

---
+++ Dates/Date.js
@@ -0,0 +1,3 @@
+// date function has = year,month,day,hour,minutes,seconds,ms
+const date=new Date();
+console.log(date);
---
+++ Dates/Date1.js
@@ -0,0 +1,14 @@
+// date function has = year,month,day,hour,minutes,seconds,ms
+const date=new Date();
+const year=date.getFullYear();
+const month=date.getMonth();
+const day=date.getDate();
+const hour=date.getHours();
+const minutes=date.getMinutes();
+const sec=date.getSeconds();
+console.log(year);
+console.log(month);
+console.log(day);
+console.log(hour);
+console.log(minutes);
+console.log(sec);
