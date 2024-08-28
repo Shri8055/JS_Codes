faffd7eeb3ae3b6f9ca210d5c8e62c54fdd349d9
Shrinivas Mahendra Kangralkar <88592860+Shri8055@users.noreply.github.com>
2024-08-28 15:33:46 +0530

    Add files via upload

---
+++ ES6Modules/Module1.js
@@ -0,0 +1,7 @@
+export const PI=3.14159;
+export function area(radius){
+    return PI*radius*radius;
+}
+export function circumferance(radius){
+    return 2*PI*radius;
+}
---
+++ ES6Modules/index.js
@@ -0,0 +1,6 @@
+import {PI,area,circumferance} from './Module1.js';
+console.log(PI);
+const areaOfcircle=area(10);
+console.log(`Area of circle is: ${areaOfcircle}cm`);
+const circum=circumferance(10);
+console.log(`Area of circle is: ${circum}cm`);
