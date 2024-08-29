7b85fde79a4aa787794234840a0b6039a3ffad0a
Shrinivas Mahendra Kangralkar <88592860+Shri8055@users.noreply.github.com>
2024-08-29 18:04:26 +0530

    Delete DOM directory

--- DOM/DOM/FirstDOM.js
+++
@@ -1,9 +0,0 @@
-// first DOM manipulation in HTML
-console.dir(document);
-document.title="My First DOM"
-document.body.style.backgroundColor="Yellow"
-
-// second DOM manipulation
-const username="Shrinivas";
-const welcomeMsg=document.getElementById("welcome-msg");
-welcomeMsg.textContent+=username == "" ? 'Guest' : username;
