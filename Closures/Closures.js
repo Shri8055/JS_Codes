6c2dd16bcf54c81c00f34a88e52edca60f70d625
Shrinivas Mahendra Kangralkar <88592860+Shri8055@users.noreply.github.com>
2024-09-01 21:41:45 +0530

    Update Closures.js

--- Closures/Closures.js
+++ Closures/Closures.js
@@ -1,6 +1,6 @@
 // function inside function, variables declared inside function remains in that functions only
 function outer(){
-    let msg="shrinivas";
+    let msg="shrinivas Kangralkar";
     function inner(){
         console.log(msg);        
     }
