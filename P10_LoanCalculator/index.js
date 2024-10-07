ac8b6bdd290f4d830e7d2f695bdb41edb907a044
Shrinivas Mahendra Kangralkar <88592860+Shri8055@users.noreply.github.com>
2024-10-07 15:27:12 +0530

    Delete P10_LoanCalculator directory

--- P10_LoanCalculator/index.js
+++
@@ -1,8 +0,0 @@
-function calc(){
-    loanAmountValue=document.getElementById("loan-amount").value;
-    interestRateValue=document.getElementById("interest-rate").value;
-    monthsToPayValue=document.getElementById("months-to-pay").value;
-    interest=(loanAmountValue*(interestRateValue*0.01))/monthsToPayValue;
-    monthlypayment=(loanAmountValue/monthsToPayValue+interest).toFixed(2);
-    document.getElementById("EMI").innerHTML=`Monthly Payment : ${monthlypayment}`;
-}
